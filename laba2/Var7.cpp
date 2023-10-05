e707cd24dfe87abce327e3d0c8ef64fa409ac5a6
barty <147055286+bartyyyy@users.noreply.github.com>
2023-10-05 20:20:37 +0300

    Add files via upload

---
+++ laba2/Var7.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+void main()
+{
+	double y, w, x = 1.4, m = 6, z = 0.05e-5;
+	y = sqrt(1 + x) - cos(2 / m);
+	w = 0.6 * z - 2*exp( - 2 * y * m);
+	std::cout << 'y= ' << y;
+	std::cout << 'w= ' << w;
+}
